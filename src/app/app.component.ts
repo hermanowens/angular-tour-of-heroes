e42cfc163dc2d8b6a6d58a090ad546edebb9803f
Herman Owens <herman.owens3@gmail.com>
2017-10-24 23:25:48 -0400

    Separates the components into separates files.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
 
+import {Hero} from './hero';
+
 const HEROES: Hero[] = [
   {id: 11, name: 'Mr. Nice'},
   {id: 12, name: 'Narco'},
@@ -12,11 +14,6 @@ const HEROES: Hero[] = [
   {id: 19, name: 'Magma'},
   {id: 20, name: 'Tornado'}
 ];
-
-export class Hero {
-  id: number;
-  name: string;
-}
 
 @Component({
   selector: 'app-root',
